8440d9e7dab93b616fc28351463d65eba3727e86
Marcel Breyer <breyer.marcel@web.de>
2021-06-06 17:59:49 +0200

    Added Catch2 testing framework.

---
+++ tests/types.cpp
@@ -0,0 +1,8 @@
+/**
+ * Copyright (C) 2021-06-05 - Marcel Breyer - All Rights Reserved
+ * Licensed under the MIT License. See LICENSE.md file in the project root for full license information.
+ *
+ * Implements tests for the type aliases of the cpp_util::dynarray class.
+ */
+
+#include "catch/catch.hpp"
