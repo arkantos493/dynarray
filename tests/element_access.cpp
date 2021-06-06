958f01f5697cec0a5d6db6336f51d33392141a4f
Marcel Breyer <breyer.marcel@web.de>
2021-06-06 20:46:42 +0200

    Added test cases for element access member functions.

---
+++ tests/element_access.cpp
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2021-06-06 - Marcel Breyer - All Rights Reserved
+ * Licensed under the MIT License. See LICENSE.md file in the project root for full license information.
+ *
+ * Implements tests for the element access member functions of the cpp_util::dynarray class.
+ */
+
+#include "dynarray.hpp"
+#include "catch/catch.hpp"
+
+
+TEST_CASE("dynarray element access member functions", "[element_access]") {
+  cpp_util::dynarray<int> arr = { 0, 1, 2, 3, 4, 5 };
+  const cpp_util::dynarray<int> const_arr = { 0, 1, 2, 3, 4, 5 };
+  cpp_util::dynarray<int> default_arr;
+
+  REQUIRE(arr.size() == 6);
+  REQUIRE(const_arr.size() == 6);
+
+  SECTION("at() member function") {
+    for (std::size_t i = 0; i < 6; ++i) {
+      CHECK(arr.at(i) == i);
+      CHECK(const_arr.at(i) == i);
+    }
+
+    CHECK_THROWS_AS(arr.at(6), std::out_of_range);
+    CHECK_THROWS_AS(const_arr.at(6), std::out_of_range);
+    CHECK_THROWS_AS(default_arr.at(0), std::out_of_range);
+  }
+
+  SECTION("operator[] member function") {
+    for (std::size_t i = 0; i < 6; ++i) {
+      CHECK(arr[i] == i);
+      CHECK(const_arr[i] == i);
+    }
+  }
+
+  SECTION("front() member function") {
+    CHECK(arr.front() == 0);
+    CHECK(const_arr.front() == 0);
+  }
+
+  SECTION("back() member function") {
+    CHECK(arr.back() == 5);
+    CHECK(const_arr.back() == 5);
+  }
+
+  SECTION("data() member function") {
+    CHECK(arr.data() == &arr[0]);
+    CHECK(const_arr.data() == &const_arr[0]);
+  }
+
+}
